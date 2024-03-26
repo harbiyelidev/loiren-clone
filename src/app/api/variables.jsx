c2be1523233791d15e6fcc357c362e5cfa442b56
Vezir Oni <105018824+harbiyelim@users.noreply.github.com>
2024-03-26 12:07:20 +0300

    Update variables.jsx

--- src/app/api/variables.jsx
+++ src/app/api/variables.jsx
@@ -6,8 +6,16 @@ export const products = [
         "id": 1,
         "name": "money-laundering",
         "description": "Money laundering script for Fivem.",
-        "price": "3.49",
+        "price": "2.49",
         "version": "v1.0.0",
         "upDate": "24.03.2024"
+    },
+    {
+        "id": 2,
+        "name": "donate-car",
+        "description": "Special vehicle export menu for Fivem",
+        "price": "2.49",
+        "version": "v1.0.0",
+        "upDate": "26.03.2024"
     },
 ]
