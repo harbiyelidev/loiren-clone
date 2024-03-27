1e495bfa3356e5fe325823deede94223a1d8da62
Vezir Oni <105018824+harbiyelim@users.noreply.github.com>
2024-03-27 15:32:46 +0300

    Update variables.jsx

--- src/app/api/variables.jsx
+++ src/app/api/variables.jsx
@@ -6,7 +6,7 @@ export const products = [
         "id": 1,
         "name": "money-laundering",
         "description": "Money laundering script for Fivem.",
-        "price": "2.49",
+        "price": "80₺",
         "version": "v1.0.0",
         "upDate": "24.03.2024"
     },
@@ -14,8 +14,40 @@ export const products = [
         "id": 2,
         "name": "donate-car",
         "description": "Special vehicle export menu for Fivem",
-        "price": "2.49",
+        "price": "80₺",
         "version": "v1.0.0",
         "upDate": "26.03.2024"
+    },
+    {
+        "id": 3,
+        "name": "discord-ticket",
+        "description": "Server-specific premium ticket bot for Discord",
+        "price": "130₺",
+        "version": "v1.0.0",
+        "upDate": "27.03.2024"
+    },
+    {
+        "id": 4,
+        "name": "discord-moderation",
+        "description": "Server-specific premium moderation bot for Discord",
+        "price": "130₺",
+        "version": "v1.0.0",
+        "upDate": "27.03.2024"
+    },
+    {
+        "id": 5,
+        "name": "discord-register",
+        "description": "Server-specific premium register bot for Discord",
+        "price": "100₺",
+        "version": "v1.0.0",
+        "upDate": "27.03.2024"
+    },
+    {
+        "id": 6,
+        "name": "discord-log",
+        "description": "Server-specific premium log bot for Discord",
+        "price": "80₺",
+        "version": "v1.0.0",
+        "upDate": "27.03.2024"
     },
 ]
